a2f36c29b436c5a174c21e9a266bad209ca82009
Mahmoud Hassan <mahmoudhassan@pop-os.localdomain>
2021-06-27 21:07:26 +0200

    update sine-wave component

---
+++ src/components/SineWave/index.js
@@ -0,0 +1,38 @@
+import styles from "./index.module.scss";
+
+export default function SineWave({
+  title,
+  step,
+  primaryDesc,
+  secondaryDesc,
+  img,
+  RTL = false,
+  background = "#ffffff",
+}) {
+  return (
+    <section
+      className={`${styles.wave} container-fluid`}
+      style={{ backgroundColor: background }}
+    >
+      <div className="container">
+        <div className={`${RTL ? "flex-row-reverse" : ""} row mx-5 align-items-center`}>
+          <div className={`col-sm-12 col-md-5 p-0 ${RTL ? "ps-5" : ""}`}>
+            <h2 className="text-uppercase mb-3">
+              <span className="pe-2">
+                0<span className="text-primary">{step}</span>
+              </span>
+              {title}
+            </h2>
+            <p>{primaryDesc}</p>
+            {secondaryDesc ? <p>{secondaryDesc}</p> : null}
+          </div>
+          <div className="col-sm-12 col-md-7 p-0">
+            <div className={`${styles.waveImg}`}>
+              <img src={img} alt={`${title} img`} loading="lazy" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/SakneenCase/index.js
+++ src/pages/SakneenCase/index.js
@@ -1,5 +1,6 @@
 import Cover from "../../components/Cover";
 import Status from "../../components/Status";
+import SineWave from "../../components/SineWave";
 
 export default function SakneenCase() {
   const projectStatus = [
@@ -18,26 +19,44 @@ export default function SakneenCase() {
   ];
 
   return (
-    <div className="container">
-      <div className="mx-5 mt-5">
-        <button type="button" className="btn btn-primary">
-          back
-        </button>
-        <Cover
-          title="sakneen"
-          subTitle="Buy or sell homes in Egypt"
-          strong="Sakneen, the online portal"
-          bio="that helps home buyers search for properties, key financial service providers and prominent angel investors spanning Egypt, MENA, the US, and Europe."
-          coverImg="/assets/img.jpg"
-        />
-        <div className="row justify-content-center mt-5">
-          {projectStatus.map((item, index) => (
-            <div key={index} className="col-sm-12 col-md-3">
-              <Status title={item.title} bio={item.bio} />
-            </div>
-          ))}
+    <>
+      <div className="container">
+        <div className="mx-5 mt-5">
+          <button type="button" className="btn btn-primary">
+            back
+          </button>
+          <Cover
+            title="sakneen"
+            subTitle="Buy or sell homes in Egypt"
+            strong="Sakneen, the online portal"
+            bio="that helps home buyers search for properties, key financial service providers and prominent angel investors spanning Egypt, MENA, the US, and Europe."
+            coverImg="/assets/img.jpg"
+          />
+          <div className="row justify-content-center mt-5">
+            {projectStatus.map((item, index) => (
+              <div key={index} className="col-sm-12 col-md-3">
+                <Status title={item.title} bio={item.bio} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+      <SineWave
+        title="problem"
+        step="0"
+        primaryDesc="But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain"
+        secondaryDesc="was born and I will give you a complete account of the system, and expound the actual teachings of th"
+        img="/assets/img2.jpg"
+      />
+      <SineWave
+        title="solution"
+        step="1"
+        primaryDesc="But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain"
+        secondaryDesc="was born and I will give you a complete account of the system"
+        img="/assets/img.jpg"
+        background="#f4f4f4"
+        RTL={true}
+      />
+    </>
   );
 }
