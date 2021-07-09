6bd7945a139b54a48fdc7327bc72af49147cb965
Mahmoud Hassan <mahmoudhassan@pop-os.localdomain>
2021-07-09 23:16:42 +0200

    update back button

--- src/components/BackBtn/index.js
+++ src/components/BackBtn/index.js
@@ -1,9 +1,10 @@
+import styles from "./index.module.scss";
 import { Link } from "react-router-dom";
 
 export default function BackButton({ url }) {
   return (
     <Link to={url}>
-      <button type="button" className="btn btn-link ps-0">
+      <button type="button" className={`${styles.Backbtn} btn`}>
         <img src="../assets/back.svg" alt="back button" loading="lazy" />
       </button>
     </Link>
